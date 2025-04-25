296374e74eb58a36cfbc05a618a0f8b71c2449ea
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-25 13:55:22 -0400

    Add listing of posts to show page

--- app/Http/Controllers/DiscussionShowController.php
+++ app/Http/Controllers/DiscussionShowController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\DiscussionResource;
+use App\Http\Resources\PostResource;
 use App\Models\Discussion;
+use App\Models\Post;
 
 class DiscussionShowController extends Controller
 {
@@ -13,6 +15,12 @@ class DiscussionShowController extends Controller
 
         return inertia()->render('Forum/Show', [
             'discussion' => DiscussionResource::make($discussion),
+            'posts' => PostResource::collection(
+                Post::whereBelongsTo($discussion)
+                    ->with(['user', 'discussion'])
+                    ->oldest()
+                    ->paginate(10)
+            ),
         ]);
     }
 }
