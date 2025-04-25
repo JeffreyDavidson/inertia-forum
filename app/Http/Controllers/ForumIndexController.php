71387d6f7fdb7c7fd2896e057fe2de0147e3cac2
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-24 20:21:29 -0400

    Setup forum page

---
+++ app/Http/Controllers/ForumIndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ForumIndexController extends Controller
+{
+    public function __invoke()
+    {
+        return inertia()->render('Forum/Index');
+    }
+}
