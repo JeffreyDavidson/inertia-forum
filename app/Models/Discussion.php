0cc48effbe401b96e59d69caa3181d2916a6ca6e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-25 13:42:43 -0400

    Create posts for a discussion

--- app/Models/Discussion.php
+++ app/Models/Discussion.php
@@ -5,12 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Discussion extends Model
 {
     public function topic(): BelongsTo
     {
         return $this->belongsTo(Topic::class);
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function post(): HasOne
+    {
+        return $this->hasOne(Post::class)
+            ->whereNull('parent_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belognsTo(User::class);
     }
 
     public function isPinned()
