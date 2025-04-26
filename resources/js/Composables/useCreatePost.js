c295ed1c2257f89fc9cccd3e4f37b0b8f6f50aa4
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-26 11:53:23 -0400

    Add post create

---
+++ resources/js/Composables/useCreatePost.js
@@ -0,0 +1,28 @@
+import { ref } from 'vue'
+import { useForm } from '@inertiajs/vue3'
+
+const visible = ref(false)
+const discussion = ref(null)
+
+const form = useForm({
+    body: ''
+})
+
+export default () => {
+    const showCreatePostForm = (discussionContext) => {
+        discussion.value = discussionContext;
+        visible.value = true;
+    }
+
+    const hideCreatePostForm = () => {
+        visible.value = false
+    }
+
+    return {
+        form,
+        visible,
+        showCreatePostForm,
+        hideCreatePostForm,
+        discussion
+    }
+}
--- resources/js/Pages/Forum/Show.vue
+++ resources/js/Pages/Forum/Show.vue
@@ -3,8 +3,12 @@ import ForumLayout from '@/Layouts/ForumLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import Post from '@/Components/Forum/Post.vue';
 import Pagination from '@/Components/Pagination.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import pluralize from 'pluralize';
 import Navigation from '@/Components/Forum/Navigation.vue';
+import useCreatePost from '@/Composables/useCreatePost';
+
+const { showCreatePostForm } = useCreatePost();
 
 defineProps({
     discussion: Object,
@@ -43,6 +47,9 @@ defineProps({
         </div>
 
         <template #side>
+            <PrimaryButton v-on:click="showCreatePostForm(discussion)" class="w-full flex justify-center h-10" v-if="$page.props.auth.user">
+                Reply to discussion
+            </PrimaryButton>
             <Navigation :query="query"/>
         </template>
     </ForumLayout>
