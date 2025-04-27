a502bf09706f5ef140871a14ea7213e173ec78ff
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-27 16:58:50 -0400

    Add better styling for no users found

--- resources/js/Components/Forum/CreateDiscussionForm.vue
+++ resources/js/Components/Forum/CreateDiscussionForm.vue
@@ -36,6 +36,9 @@
                 <InputLabel for="body" value="Body" class="sr-only"/>
                 <Mentionable :keys="['@']" offset="6" v-on:search="mentionSearch" :items="mentionSearchResults" v-if="!markdownPreviewEnabled">
                     <Textarea v-if="!markdownPreviewEnabled" id="body" class="w-full h-48 align-top" v-model="form.body"/>
+                    <template #no-result>
+                        <div class="mention-item">No username found</div>
+                    </template>
                 </Mentionable>
                 <InputError class="mt-2" :message="form.errors.body"/>
             </div>
