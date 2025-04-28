e84ff4739a420b455cebf2cbf03f7cce80ce73fd
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-04-28 12:09:34 -0400

    Ensure post belongs to a discussion before marking solution

--- app/Http/Controllers/DiscussionSolutionPatchController.php
+++ app/Http/Controllers/DiscussionSolutionPatchController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\DiscussionSolutionPatchRequest;
 use App\Models\Discussion;
-use App\Models\Post;
 
 class DiscussionSolutionPatchController extends Controller
 {
     public function __invoke(DiscussionSolutionPatchRequest $request, Discussion $discussion)
     {
-        $discussion->solution()->associate(Post::find($request->post_id));
+        $discussion->solution()->associate($discussion->posts()->find($request->post_id));
         $discussion->save();
 
         return back();
